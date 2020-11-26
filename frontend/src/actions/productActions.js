f7cac923b132558f33cff8007c5d3472233cc62d
msfarookhy <msfarookhy@gmail.com>
2020-11-26 12:33:00 +0530

    map error resolve

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -18,7 +18,7 @@ export const listProducts = () => async (dispatch) => {
     dispatch({
       type: PRODUCT_LIST_FAIL,
       payload:
-        error.response && error.data.message
+        error.response && error.response.data.message
           ? error.response.data.message
           : error.message,
     });
