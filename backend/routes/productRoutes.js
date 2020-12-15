118e95be505281addd06d36b9871c551df8bad58
msfarookhy <msfarookhy@gmail.com>
2020-12-15 23:55:23 +0530

    Authenctication And JSON web Token Added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,37 +1,11 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
 const router = express.Router();
-import Product from "../models/productModel.js";
+import {
+  getProductById,
+  getProducts,
+} from "../controllers/productController.js";
 
-//@des fetch all products
-//@des Get /api/products
-//@access  Public
-
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    // throw new Error("some Error");
-    res.json(products);
-  })
-);
-
-//@des fetch single products
-//@des Get /api/products/:id
-//@access  Public
-
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404).json({ message: "Product Not Found Chutiye" });
-    }
-    // res.json(product);
-  })
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 
 export default router;
