19d514a57727dd9020bfb9bc399aa5ea40d8a974
msfarookhy <msfarookhy@gmail.com>
2020-12-19 03:36:13 +0530

    Place Order Screen & Created  Order Controller & Route in backend

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,37 @@
+import asyncHandler from "express-async-handler";
+import Order from "../models/orderModel.js";
+
+//@des Create new Order
+//@des Get /api/orders
+//@access  Private
+const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order Items");
+    return;
+  } else {
+    const order = new Order({
+      orderItems,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+    const createdOrder = await order.save();
+    res.status(201).json(createdOrder);
+  }
+});
+
+export { addOrderItems };
