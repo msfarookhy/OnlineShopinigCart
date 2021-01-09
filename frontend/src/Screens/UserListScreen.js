8b07c7cb15e48f28d33ea4b305b60806023c6bfe
msfarookhy <msfarookhy@gmail.com>
2021-01-10 00:04:51 +0530

    admin user delet

--- frontend/src/Screens/UserListScreen.js
+++ frontend/src/Screens/UserListScreen.js
@@ -5,15 +5,22 @@ import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message.js";
 import Loader from "../components/Loader.js";
 import { listUsers } from "../actions/userActions";
-const UserListScreen = () => {
+const UserListScreen = ({ history }) => {
   const dispatch = useDispatch();
 
   const userList = useSelector((state) => state.userList);
   const { loading, error, users } = userList;
 
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
   useEffect(() => {
-    dispatch(listUsers());
-  }, [dispatch]);
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listUsers());
+    } else {
+      history.push("/login");
+    }
+  }, [dispatch, history]);
 
   const deleteHandler = (id) => {
     console.log("delet");
